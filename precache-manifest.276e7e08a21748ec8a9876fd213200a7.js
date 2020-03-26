7336bd4d428047bf94fd947578581a4622d31df2
Yuli Hardina <yulihardinap@gmail.com>
2020-03-26 18:46:46 +0800

    Updates

---
+++ precache-manifest.276e7e08a21748ec8a9876fd213200a7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a526359384c0088f1e7ea65af4b921c4",
+    "url": "/WEB3B_432185_YuliHardina_React/index.html"
+  },
+  {
+    "revision": "e1de5c33485c3235c43e",
+    "url": "/WEB3B_432185_YuliHardina_React/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "0dbeb2349156430b600e",
+    "url": "/WEB3B_432185_YuliHardina_React/static/css/main.f97b3766.chunk.css"
+  },
+  {
+    "revision": "e1de5c33485c3235c43e",
+    "url": "/WEB3B_432185_YuliHardina_React/static/js/2.11a662f6.chunk.js"
+  },
+  {
+    "revision": "3a411c08ae28e3caa08c7bca646fcfd8",
+    "url": "/WEB3B_432185_YuliHardina_React/static/js/2.11a662f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbeb2349156430b600e",
+    "url": "/WEB3B_432185_YuliHardina_React/static/js/main.ae78d3fc.chunk.js"
+  },
+  {
+    "revision": "7905f8b9d124f2c715ef",
+    "url": "/WEB3B_432185_YuliHardina_React/static/js/runtime-main.c9ae6070.js"
+  },
+  {
+    "revision": "7d7e97de3eea6da2c5aa6655bee5d54a",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/Profile picture.7d7e97de.png"
+  },
+  {
+    "revision": "0a799148a50bb02c6f380eabd8d97559",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/fontawesome-webfont.0a799148.svg"
+  },
+  {
+    "revision": "3293616ec0c605c7c2db25829a0a509e",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/fontawesome-webfont.3293616e.woff"
+  },
+  {
+    "revision": "8b27bc96115c2d24350f0d09e6a9433f",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/fontawesome-webfont.8b27bc96.eot"
+  },
+  {
+    "revision": "dcb26c7239d850266941e80370e207c1",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/fontawesome-webfont.dcb26c72.ttf"
+  },
+  {
+    "revision": "aed488942e587b9ba35c6d1f98c64f5d",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/header-bg.aed48894.jpg"
+  },
+  {
+    "revision": "1740aa2859ea21698aaace4329e151aa",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/overlay.1740aa28.jpg"
+  },
+  {
+    "revision": "d07207f1e7ab04ea1cfc1f8bbe1d8d31",
+    "url": "/WEB3B_432185_YuliHardina_React/static/media/web-kmhd.d07207f1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yulihardinaa/WEB3B_432185_YuliHardina_React/precache-manifest.ace4e5cc19e2385d2443b2904f64fff5.js"
+  "/WEB3B_432185_YuliHardina_React/precache-manifest.276e7e08a21748ec8a9876fd213200a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yulihardinaa/WEB3B_432185_YuliHardina_React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WEB3B_432185_YuliHardina_React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
